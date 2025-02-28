7fa02ee845c343a1a5d1677ea05bde541dd29ba7
DragonautMk <michelesavarese1997@gmail.com>
2025-02-28 18:23:29 +0100

    Add initial project structure with authentication features and styling

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">MyFiance</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03"
+            aria-controls="navbarsExample03" aria-expanded="true" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse show" id="navbarsExample03" style="">
+            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">I nostri servizi finanziari</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">I nostro Staff</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+                        aria-expanded="false">Area Clienti</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Accedi</a></li>
+                        <li><a class="dropdown-item" href="#">Prenota un appuntamento</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">La nostra storia</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Info e contatti</a>
+                </li>
+            </ul>
+            <form role="search">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+    </div>
+</nav>
